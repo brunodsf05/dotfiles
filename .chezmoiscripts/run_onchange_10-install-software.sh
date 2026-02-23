bf24353aaccc796022169870957e0ba11912e6d8
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2026-02-23 19:37:49 +0100

    Feat: Added script to install my software

---
+++ .chezmoiscripts/run_onchange_10-install-software.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+# --- Utilities --- #
+
+dnf_install() {
+    local missing=()
+
+    # Collect missing packages
+    for pkg in "$@"; do
+        rpm -q "$pkg" >/dev/null 2>&1 || missing+=("$pkg")
+    done
+
+    # Install at once the missing
+    if [ "${#missing[@]}" -gt 0 ]; then
+        printf "Installing dnf:\n${missing[*]}\n"
+        sudo dnf install -y --skip-unavailable "${missing[@]}"
+    else
+        echo "All packages already installed"
+    fi
+}
+
+flatpak_install() {
+    local missing=()
+
+    # Collect missing Flatpak apps
+    for app in "$@"; do
+        flatpak info "$app" >/dev/null 2>&1 || missing+=("$app")
+    done
+
+    # Install at once the missing
+    if [ "${#missing[@]}" -gt 0 ]; then
+        printf "Installing flatpak:\n${missing[*]}\n"
+        flatpak install -y flathub "${missing[@]}"
+    else
+        echo "All flatpak apps already installed"
+    fi
+}
+
+# --- Install packages --- #
+
+dnf_packages=(
+    # --- Art --- #
+    "gimp"
+    "inkscape"
+    # --- Development --- #
+    "nodejs"
+    "pnpm"
+    "zed"
+    # --- Fonts --- #
+    "cousine-nerd-fonts"
+    "jetbrainsmono-nerd-fonts"
+    "departuremono-nerd-fonts"
+    # --- Terminal --- #
+    "eza"
+    "fastfetch"
+    "fish"
+    "fzf"
+    "kitty"
+    "micro"
+    "starship"
+    "yazi"
+    "zellij"
+    "zoxide"
+)
+flatpak_packages=(
+    # --- Development --- #
+    "io.github.MakovWait.Godots"
+    # --- Entertainment --- #
+    "com.heroicgameslauncher.hgl"
+    "com.stremio.Stremio"
+    "io.github.Faugus.faugus-launcher"
+    "org.prismlauncher.PrismLauncher"
+    # --- Tools --- #
+    "io.gitlab.theevilskeleton.Upscaler"
+    "org.chromium.Chromium"
+    "org.keepassxc.KeePassXC"
+    "org.mozilla.Thunderbird"
+    "org.qbittorrent.qBittorrent"
+)
+
+dnf_install "${dnf_packages[@]}"
+flatpak_install "${flatpak_packages[@]}"
+
+# --- Configuration --- #
+
+sudo chsh -s /usr/bin/fish $USER
