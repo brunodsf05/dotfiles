700d78d0b92f7ff8a447bb73f6704e53984d8211
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2026-02-23 12:55:58 +0100

    Feat: The script setup-git prints what task is doing at the moment

--- .chezmoiscripts/run_onchange_setup-git.sh
+++ .chezmoiscripts/run_onchange_setup-git.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+echo "Installing git and utilities..."
 rpm -q git || sudo dnf install -y git
 rpm -q gh || sudo dnf install -y gh
 rpm -q git-filter-repo || sudo dnf install -y git-filter-repo
 
+echo "Configuring git..."
 git config --global user.name "brunodsf05"
 git config --global user.email "231746160+brunodsf05@users.noreply.github.com"
 git config --global init.defaultBranch "main"
