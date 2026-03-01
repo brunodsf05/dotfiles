85e72dd39d1f93001db46f331a34f6db3be1e0ff
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2026-03-01 17:53:55 +0100

    Feat: setup-gpu installs and configures nvidia-container-toolkit to allow podman the usage of MY gpu

--- .chezmoiscripts/run_onchange_01-setup-gpu.sh
+++ .chezmoiscripts/run_onchange_01-setup-gpu.sh
@@ -7,3 +7,15 @@ set -e
 echo "Installing NVIDIA drivers..."
 rpm -q akmod-nvidia || sudo dnf install -y akmod-nvidia
 rpm -q xorg-x11-drv-nvidia-cuda || sudo dnf install -y xorg-x11-drv-nvidia-cuda
+
+echo "Configuring podman integration..."
+rpm -q golang-github-nvidia-container-toolkit || sudo dnf install -y nvidia-container-toolkit
+NVIDIA_CDI_FILE="/etc/cdi/nvidia.yaml"
+if [ ! -f "$NVIDIA_CDI_FILE" ]; then
+  echo "Generating NVIDIA CDI config..."
+  sudo nvidia-ctk cdi generate --output="$NVIDIA_CDI_FILE"
+echo "Done!"
+else
+  echo "NVIDIA CDI config already exists"
+fi
+sudo setsebool -P container_use_devices true
