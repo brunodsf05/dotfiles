2bcf44e0fb4eed44f8175f847a5cd1f24f3aea54
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2026-03-11 14:03:57 +0100

    Feat: Added custom kde's konsole config

--- .chezmoiscripts/run_onchange_10-install-software.sh
+++ .chezmoiscripts/run_onchange_10-install-software.sh
@@ -88,3 +88,4 @@ flatpak_install "${flatpak_packages[@]}"
 # --- Configuration --- #
 
 sudo chsh -s /usr/bin/fish $USER
+rpm -q konsole kf6-kconfig && kwriteconfig6 --file konsolerc --group "Desktop Entry" --key DefaultProfile Mine.profile
