c8f090dd6b648cddcb5d871e04a2e3c04d59dffc
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2026-05-08 18:22:30 +0200

    Feat: Disable dnf optimizations

--- .chezmoiscripts/run_onchange_00-setup-software-sources.sh
+++ .chezmoiscripts/run_onchange_00-setup-software-sources.sh
@@ -4,11 +4,12 @@ set -e
 
 # --- RPM --- #
 
-echo "Making DNF faster..."
-CONF="/etc/dnf/dnf.conf"
-grep -q "^\[main\]" "$CONF" || echo "[main]" | sudo tee -a "$CONF"
-grep -q "^max_parallel_downloads=" "$CONF" || echo "max_parallel_downloads=20" | sudo tee -a "$CONF"
-grep -q "^fastestmirror=" "$CONF" || echo "fastestmirror=True" | sudo tee -a "$CONF"
+# --- Investigate if it is really needed ---
+# echo "Making DNF faster..."
+# CONF="/etc/dnf/dnf.conf"
+# grep -q "^\[main\]" "$CONF" || echo "[main]" | sudo tee -a "$CONF"
+# grep -q "^max_parallel_downloads=" "$CONF" || echo "max_parallel_downloads=20" | sudo tee -a "$CONF"
+# grep -q "^fastestmirror=" "$CONF" || echo "fastestmirror=True" | sudo tee -a "$CONF"
 
 echo "Adding Terra repositories..."
 rpm -q terra-release || sudo dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
