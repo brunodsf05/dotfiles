23b8d96fa78ee1739b872f865456a037e47dd787
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2026-06-14 12:36:30 +0200

    Feat: Change git user.email

--- .chezmoiscripts/run_onchange_20-setup-git.sh
+++ .chezmoiscripts/run_onchange_20-setup-git.sh
@@ -6,6 +6,6 @@ rpm -q git || { echo "Git configuration skipped because it is not installed."; e
 
 echo "Configuring git..."
 git config --global user.name "brunodsf05"
-git config --global user.email "231746160+brunodsf05@users.noreply.github.com"
+git config --global user.email "brunodsf05.dev@gmail.com"
 git config --global init.defaultBranch "main"
 git config --global alias.authors 'shortlog -s -n -e'
