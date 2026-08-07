0082a0b95258f11381aec89b864036b50b67ec06
brunodsf05 <brunodsf05.dev@gmail.com>
2026-08-07 20:24:16 +0200

    Feat: Merge rpm-fusion and flatpak install into a single "dnf install -y" command

--- .chezmoiscripts/run_onchange_00-setup-software-sources.sh
+++ .chezmoiscripts/run_onchange_00-setup-software-sources.sh
@@ -2,23 +2,20 @@
 
 set -e
 
-# --- RPM --- #
-
-# --- Investigate if it is really needed ---
+# --- General --- #
 echo "Making DNF faster... (/etc/dnf/dnf.conf)"
 sudo dnf5 config-manager setopt max_parallel_downloads=10 fastestmirror=True
 
 # echo "Adding Terra repositories..."
 # rpm -q terra-release || sudo dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 
-echo "Adding RPM Fusion..."
-rpm -q rpmfusion-free-release || sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-rpm -q rpmfusion-nonfree-release || sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+echo "Installing RPM Fusion and Flatpak..."
+sudo dnf install -y \
+    "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+    "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" \
+    flatpak
 
 # --- Flatpak --- #
-
-echo "Installing flatpak..."
-rpm -q flatpak || sudo dnf install -y flatpak
 
 echo "Disabling fedora's flatpak repo..."
 flatpak remotes | grep "fedora" && sudo flatpak remote-modify --disable fedora
