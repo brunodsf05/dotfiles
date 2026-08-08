e98fc3edcc74ac47d05f171d124d6a3d8b57b790
brunodsf05 <brunodsf05.dev@gmail.com>
2026-08-08 19:27:41 +0200

    Fix: Set correct thunderbird flatpak-id

--- chezmoi/.chezmoiscripts/run_onchange_11-install-software-flatpak.sh
+++ chezmoi/.chezmoiscripts/run_onchange_11-install-software-flatpak.sh
@@ -12,26 +12,25 @@ to_install=(
     "io.gitlab.theevilskeleton.Upscaler"
     "org.chromium.Chromium"
     "org.keepassxc.KeePassXC"
-    "org.mozilla.Thunderbird"
+    "org.mozilla.thunderbird_esr"
     "org.mozilla.firefox"
     "org.qbittorrent.qBittorrent"
 )
 
-flatpak_install() {
-    local missing=()
+# Generate temporal script that runs in a new terminal
+tmp_script="$(mktemp)"
+cat > "$tmp_script" <<EOF
+#!/usr/bin/env bash
 
-    # Collect missing Flatpak apps
-    for app in "$@"; do
-        flatpak info "$app" >/dev/null 2>&1 || missing+=("$app")
-    done
+flatpak install -y flathub ${to_install[*]}
 
-    # Install at once the missing
-    if [ "${#missing[@]}" -gt 0 ]; then
-        printf "Installing flatpak:\n${missing[*]}\n"
-        flatpak install -y flathub "${missing[@]}"
-    else
-        echo "All flatpak apps already installed"
-    fi
-}
+pw-play /usr/share/sounds/freedesktop/stereo/complete.oga
+printf '\\nPress Enter to close...'
+read -r
 
-flatpak_install "${to_install[@]}"
+rm -f "\$0"
+EOF
+
+chmod +x "$tmp_script"
+
+xdg-terminal-exec -- bash "$tmp_script" &
