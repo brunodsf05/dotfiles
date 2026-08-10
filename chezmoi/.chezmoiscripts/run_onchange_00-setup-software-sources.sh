b83a50558f72f92ef1d56544d921e0b0c6e61844
brunodsf05 <brunodsf05.dev@gmail.com>
2026-08-10 18:42:47 +0200

    Feat: No more nix

--- chezmoi/.chezmoiscripts/run_onchange_00-setup-software-sources.sh
+++ chezmoi/.chezmoiscripts/run_onchange_00-setup-software-sources.sh
@@ -9,18 +9,12 @@ sudo dnf5 config-manager setopt max_parallel_downloads=10 fastestmirror=True
 echo "Adding Terra repositories..."
 rpm -q terra-release || sudo dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 
-echo "Installing RPM Fusion, Flatpak and Nix..."
+echo "Installing RPM Fusion and Flatpak..."
 sudo dnf install -y \
     "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
     "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" \
     flatpak \
-    nix \
-    nix-daemon \
     xdg-terminal-exec
-
-# --- Nix --- #
-echo "Enabling nix-daemon..."
-sudo systemctl enable nix-daemon --now
 
 # --- Flatpak --- #
 echo "Disabling fedora's flatpak repo..."
